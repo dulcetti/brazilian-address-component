140620da39b3115ea466c536b9523d7dc23f4b3f
Bruno Dulcetti <brunodulcetti@gmail.com>
2020-10-29 23:15:27 -0300

    Initial configs (#1)
    * refactor: move index to examples folder
    
    * feat: add initial test and source files
    
    * feat: add npm with dev and dependencies
    
    * chore: add add initial configs
    
    * docs: add initial readme
    
    * chore: add commitlint config and husky in package

---
+++ jest.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  roots: ['<rootDir>/src'],
+  testMatch: ['**/__tests__/**/*.+(ts|tsx|js)', '**/?(*.)+(spec|test).+(ts|tsx|js)'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}', '!<rootDir>/node_modules/'],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
+  coverageReporters: ['text'],
+  testEnvironment: 'jsdom',
+  testPathIgnorePatterns: ['/node_modules/', './src/styles.scss'],
+  collectCoverage: true,
+  moduleNameMapper: {
+    '\\.(scss|css|jpg|png|gif)$': '<rootDir>/tests/files.mock.js',
+  },
+};
