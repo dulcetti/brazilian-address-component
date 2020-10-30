f2c8ceeb94f2ef7a2f8e6fa57e2ce4cc1c666533
Bruno Dulcetti <brunodulcetti@gmail.com>
2020-10-29 23:47:14 -0300

    Initial structure (#2)
    * chore: change the start script
    
    * style: add initial style from component
    
    * test: add the first test
    
    * feat: update the example html
    
    * chore: update the example folder in webpack config

---
+++ src/brazilian-address-component.test.ts
@@ -0,0 +1,7 @@
+import BrazilianAddressComponent from './brazilian-address-component';
+
+describe('Brazilian Address Component', () => {
+  test('if component should be defined', () => {
+    expect(BrazilianAddressComponent).toBeDefined();
+  });
+});
---
+++ src/brazilian-address-component.ts
@@ -0,0 +1,7 @@
+import './styles.scss';
+
+export default class BrazilianAddressComponent {
+  constructor() {
+    console.info('Entrei');
+  }
+}
