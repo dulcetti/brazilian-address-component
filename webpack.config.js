1ac32daf9beb72719fe8857ebee44b7a5b2be73c
Bruno Dulcetti <brunodulcetti@gmail.com>
2020-11-09 22:14:33 -0300

    chore: remove hmr

--- webpack.config.js
+++ webpack.config.js
@@ -25,9 +25,6 @@ module.exports = {
         use: [
           {
             loader: MiniCssExtractPlugin.loader,
-            options: {
-              hmr: process.env.NODE_ENV === 'development',
-            },
           },
           'css-loader',
           'postcss-loader',
