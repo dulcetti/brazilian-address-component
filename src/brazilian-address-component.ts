783320d5c8db0225cffeec825a034857b315c74c
Bruno Dulcetti <brunodulcetti@gmail.com>
2020-11-09 22:20:14 -0300

    Add methods to build first input (#5)
    * chore: remove hmr
    
    * feat: add input interface and your default options
    
    * feat: build input on form
    - add build input method
    - add method to set attributes of input
    - create the cep input

--- src/brazilian-address-component.ts
+++ src/brazilian-address-component.ts
@@ -1,16 +1,41 @@
 import './styles.scss';
 
-interface Options {
+interface IInput {
+  accesskey?: string;
+  id: string;
+  disabled?: boolean;
+  required?: boolean;
+  label: string;
+  maxLength?: number;
+  minLength?: number;
+  name: string;
+  placeholder: string;
+  tabIndex?: string;
+  template?: string;
+  type?: string;
+}
+
+interface IOptions {
   template?: string;
 }
 
 export default class BrazilianAddressComponent {
-  defaultOptions: Options = {
+  defaultInput: IInput = {
+    id: 'input-id',
+    disabled: false,
+    required: false,
+    label: 'Add your label here',
+    name: 'input-name',
+    placeholder: 'Add your placeholder',
+    template: 'line',
+    type: 'text',
+  };
+  defaultOptions: IOptions = {
     template: 'default',
   };
-  options: Options;
+  options: IOptions;
 
-  constructor(options: Options) {
+  constructor(options: IOptions) {
     this.options = {
       ...this.defaultOptions,
       ...options,
@@ -22,5 +47,37 @@ export default class BrazilianAddressComponent {
   private _buildComponentFields() {
     const form: HTMLFormElement = document.createElement('form');
     form.classList.add('form-address');
+
+    const inputCep = this._buildInput({
+      id: 'cep',
+      label: 'CEP da sua residência (somente números)',
+      name: 'input-cep',
+      placeholder: 'Ex: 00000-000',
+    });
+
+    form.appendChild(inputCep);
+
+    document.body.appendChild(form);
+  }
+
+  private _buildInput(input: IInput): HTMLInputElement {
+    const inputAttrs: IInput = {
+      ...this.defaultInput,
+      ...input,
+    };
+    const inputHtml: HTMLInputElement = document.createElement('input');
+    this._setAttributesOnInput(inputAttrs, inputHtml);
+
+    return inputHtml;
+  }
+
+  private _setAttributesOnInput(attrs: IInput, input: HTMLInputElement): HTMLInputElement {
+    for (const key in attrs) {
+      if (Object.prototype.hasOwnProperty.call(attrs, key)) {
+        input.setAttribute(key, attrs[key]);
+      }
+    }
+
+    return input;
   }
 }
